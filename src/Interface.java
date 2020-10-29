e6aa2a65d4ac02e43327b671ca96c9917ef74ae4
Decirius <camille.charrier.pro@outlook.fr>
2020-10-29 10:31:42 +0100

    interface

--- src/Interface.java
+++ src/Interface.java
@@ -8,10 +8,20 @@ import java.awt.GridLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.border.EtchedBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Canvas;
+import java.awt.Button;
 
 public class Interface {
 
-	private JFrame frmKingdomino;
+	private JFrame start_fen;
 
 	/**
 	 * Launch the application.
@@ -21,7 +31,7 @@ public class Interface {
 			public void run() {
 				try {
 					Interface window = new Interface();
-					window.frmKingdomino.setVisible(true);
+					window.start_fen.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -40,10 +50,61 @@ public class Interface {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmKingdomino = new JFrame();
-		frmKingdomino.setTitle("Kingdomino");
-		frmKingdomino.setBounds(100, 100, 733, 538);
-		frmKingdomino.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		start_fen = new JFrame();
+		start_fen.setTitle("Kingdomino");
+		start_fen.setBounds(100, 100, 700, 500);
+		start_fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		JPanel panel_ge = new JPanel();
+		panel_ge.setBackground(Color.LIGHT_GRAY);
+		start_fen.getContentPane().add(panel_ge, BorderLayout.CENTER);
+		panel_ge.setLayout(new GridLayout(2, 1, 0, 10));
+		
+		JPanel panel_haut = new JPanel();
+		panel_haut.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_haut.setBackground(Color.PINK);
+		panel_haut.setForeground(new Color(0, 0, 0));
+		panel_ge.add(panel_haut);
+		
+		JPanel panel_bas = new JPanel();
+		panel_ge.add(panel_bas);
+		panel_bas.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_bas.setBackground(Color.ORANGE);
+		panel_bas.setLayout(new GridLayout(0, 2, 50, 0));
+		
+		String url = "images/kingmain.png";
+		ImageIcon image = new ImageIcon(url);
+		JLabel label = new JLabel(image,JLabel.CENTER);
+		panel_haut.add(label);
+		
+		JPanel panel_bas_gauche = new JPanel();
+		panel_bas_gauche.setBackground(Color.CYAN);
+		panel_bas.add(panel_bas_gauche);
+		panel_bas_gauche.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JButton bouton_new = new JButton("Nouvelle Partie");
+		panel_bas_gauche.add(bouton_new);
+		
+		JButton btnNewButton = new JButton("Charger Partie");
+		panel_bas_gauche.add(btnNewButton);
+		
+		JPanel panel_bas_droit = new JPanel();
+		panel_bas_droit.setBackground(Color.MAGENTA);
+		panel_bas.add(panel_bas_droit);
+		panel_bas_droit.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JButton bouton_quitter = new JButton("Quitter");
+		panel_bas_droit.add(bouton_quitter);
+		
+		
+		bouton_quitter.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				start_fen.dispose();
+			}
+		});
 	}
 
 }
--- src/Starter.java
+++ src/Starter.java
@@ -1,8 +1,8 @@
 
-public class Starter {
+public class Starter  {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Fenetre_main fen = new Fenetre_main();
 
 	}
 
