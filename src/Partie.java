8ee2566be3b4b6f9787fe303b072fae8e17ef56e
Decirius <camille.charrier.pro@outlook.fr>
2020-10-29 16:17:31 +0100

    creation de la pile

--- src/Partie.java
+++ src/Partie.java
@@ -3,7 +3,56 @@ import java.util.Collections;
 import java.util.List;
 
 public class Partie {
-    private Tuile[] pile = {new Tuile(new int[] {2,1},new int[] {0,1},1),new Tuile(new int[] {4,3},new int[] {2,0},2)};
+    private Tuile[] pile = {new Tuile(new int[] {0,0},new int[] {0,0},1),
+							new Tuile(new int[] {0,0},new int[] {0,0},2),    						
+							new Tuile(new int[] {4,0},new int[] {4,0},3),    		
+							new Tuile(new int[] {4,0},new int[] {4,0},4),    		
+							new Tuile(new int[] {4,0},new int[] {4,0},5),    		
+							new Tuile(new int[] {4,0},new int[] {4,0},6),
+							new Tuile(new int[] {2,0},new int[] {2,0},7), 
+							new Tuile(new int[] {2,0},new int[] {2,0},8),
+							new Tuile(new int[] {2,0},new int[] {2,0},9),
+							new Tuile(new int[] {1,0},new int[] {1,0},10),
+							new Tuile(new int[] {1,0},new int[] {1,0},11),
+							new Tuile(new int[] {3,0},new int[] {3,0},12),
+							new Tuile(new int[] {0,0},new int[] {4,0},13),
+							new Tuile(new int[] {0,0},new int[] {2,0},14),
+							new Tuile(new int[] {0,0},new int[] {1,0},15),
+							new Tuile(new int[] {0,0},new int[] {3,0},16),
+							new Tuile(new int[] {4,0},new int[] {2,0},17),
+							new Tuile(new int[] {4,0},new int[] {1,0},18),
+							new Tuile(new int[] {0,1},new int[] {4,0},19),
+							new Tuile(new int[] {0,1},new int[] {2,0},20),
+							new Tuile(new int[] {0,1},new int[] {1,0},21),
+							new Tuile(new int[] {0,1},new int[] {3,0},22),
+							new Tuile(new int[] {0,1},new int[] {5,0},23),
+							new Tuile(new int[] {4,1},new int[] {0,0},24),
+							new Tuile(new int[] {4,1},new int[] {0,0},25),
+							new Tuile(new int[] {4,1},new int[] {0,0},26),
+							new Tuile(new int[] {4,1},new int[] {0,0},27),
+							new Tuile(new int[] {4,1},new int[] {2,0},28),
+							new Tuile(new int[] {4,1},new int[] {1,0},29),
+							new Tuile(new int[] {2,1},new int[] {0,0},30),
+							new Tuile(new int[] {2,1},new int[] {0,0},31),
+							new Tuile(new int[] {2,1},new int[] {4,0},32),
+							new Tuile(new int[] {2,1},new int[] {4,0},33),
+							new Tuile(new int[] {2,1},new int[] {4,0},34),
+							new Tuile(new int[] {2,1},new int[] {4,0},35),
+							new Tuile(new int[] {0,0},new int[] {1,1},36),
+							new Tuile(new int[] {2,0},new int[] {1,1},37),
+							new Tuile(new int[] {0,0},new int[] {3,1},38),
+							new Tuile(new int[] {1,0},new int[] {3,1},39),
+							new Tuile(new int[] {5,1},new int[] {0,0},40),
+							new Tuile(new int[] {0,0},new int[] {1,2},41),
+							new Tuile(new int[] {2,0},new int[] {1,2},42),
+							new Tuile(new int[] {0,0},new int[] {3,2},43),
+							new Tuile(new int[] {1,0},new int[] {3,2},44),
+							new Tuile(new int[] {5,2},new int[] {0,0},45),
+							new Tuile(new int[] {3,0},new int[] {5,2},46),
+							new Tuile(new int[] {3,0},new int[] {5,2},47),
+							new Tuile(new int[] {0,0},new int[] {5,3},43)};
+    
+    
     private Joueur j1;
     private Joueur j2;
     private Tuile[] tirage;
