24adeb23ea172570c204f1183315aa0943973339
pauline <pauline@pauline-ThinkPad>
2020-12-01 15:47:07 +0100

    lol c'est pas les ia

--- src/Joueur.java
+++ src/Joueur.java
@@ -8,12 +8,32 @@ public class Joueur {
     protected Tuile [] defausse;
     protected int score;
 
+    public Joueur(String nom){
+        this.grille = new Grille();
+        this.nom = nom;
+        this.reservation = new Tuile[2];
+        this.ordre = new int[2];
+        this.defausse = new Tuile[12];//nombre maximum de domino que peut avoir un joueur
+        this.score = 0;
+    }
+
     public void setGrille(Grille grille) {
         this.grille = grille;
     }
 
     public void setReservation(Tuile[] reservation) {
         this.reservation = reservation;
+    }
+
+    public void setReservation(Tuile t){
+        if(this.reservation[0] == null){
+            this.reservation[0] = t;
+        } else {
+            this.reservation[1] = t;
+        }
+    }
+    public void videReservation(){
+        this.reservation = new Tuile[2];
     }
 
     public void setOrdre(int[] ordre) {
@@ -28,13 +48,28 @@ public class Joueur {
         this.score = score;
     }
 
-    public Joueur(String nom){
-        this.grille = new Grille();
-        this.nom = nom;
-        this.reservation = new Tuile[2];
-        this.ordre = new int[2];
-        this.defausse = new Tuile[12];//nombre maximum de domino que peut avoir un joueur
-        this.score = 0;
+    public Grille getGrille() {
+        return grille;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Tuile[] getReservation() {
+        return reservation;
+    }
+
+    public int[] getOrdre() {
+        return ordre;
+    }
+
+    public Tuile[] getDefausse() {
+        return defausse;
+    }
+
+    public int getScore() {
+        return score;
     }
 
     //prend en l'id num et les deux terrains t1 et t2 de la tuile et les coord respectives de chaque terrain coord1, coord2
@@ -84,27 +119,4 @@ public class Joueur {
         this.reservation[num] = null;
     }
 
-    public Grille getGrille() {
-        return grille;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public Tuile[] getReservation() {
-        return reservation;
-    }
-
-    public int[] getOrdre() {
-        return ordre;
-    }
-
-    public Tuile[] getDefausse() {
-        return defausse;
-    }
-
-    public int getScore() {
-        return score;
-    }
 }
