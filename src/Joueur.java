a7906d1136a150cf31b7f1e95e0422431a5f0f43
Decirius <camille.charrier.pro@outlook.fr>
2020-12-02 22:29:32 +0100

    On affiche enfin bordel de merde

--- src/Joueur.java
+++ src/Joueur.java
@@ -76,27 +76,36 @@ public class Joueur {
     //retourne true si a marché
     public boolean placerTuile(int num, Coord coord1, Coord coord2){
         //trouve la position de la tuile dans la reservation
-        int i = 0;
-        for(;this.reservation[i].getId() != num;i++){
-        }
-
+        int i = num;
+        
+        System.out.println("1 : Ligne "+coord1.getLigne()+", colonne :"+coord1.getColonne());
+        System.out.println("2 : Ligne "+coord2.getLigne()+", colonne :"+coord2.getColonne());
+        
         //modifie l'orientation de la tuile selon les terrains de positionnement choisis
         //vérifie orientation 0
         if(coord1.getLigne() == coord2.getLigne() && coord1.getColonne() == (coord2.getColonne() - 1)) {
             this.reservation[i].setOrientation(0);
-        } else //vérifie position 2
+            System.out.println("orientation0");
+        } else {//vérifie position 2
             if(coord1.getLigne() == coord2.getLigne() && coord2.getColonne() == (coord1.getColonne() - 1)) {
                 this.reservation[i].setOrientation(2);
-            } else //vérifie position 1
+                System.out.println("orientation2");
+            } else {//vérifie position 1
                 if(coord1.getColonne() == coord2.getColonne() && coord1.getLigne() == (coord2.getLigne() - 1)) {
                     this.reservation[i].setOrientation(1);
-                } else //vérifie position 3
+                    System.out.println("orientation1");
+                } else {//vérifie position 3
                     if(coord1.getColonne() == coord2.getColonne() && coord2.getLigne() == (coord1.getLigne() - 1)){
                         this.reservation[i].setOrientation(3);
+                        System.out.println("orientation3");
                     } else {
                         //les terrains ne sont pas adjacents
+                    	 System.out.println("non adjacents");
                         return false;
                     }
+                }
+            }
+        }
 
         //appele la methode recevoir tuile pour positionner la tuile
         boolean succes = this.grille.recevoirTuile(this.reservation[i], coord1,coord2);
